c3a906d6835ff993d87fe2411438096e00399f40
Armchair Traveller <sentient.sober.armchair@gmail.com>
2022-12-09 09:59:31 -0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<main>
+  <h1 class="mb-8 text-center text-4xl font-bold">Hotel Booking</h1>
+  <section class="[ card-container ] flex grid-cols-2 flex-col gap-4 px-4 lg:grid 2xl:grid-cols-3">
+    {#each [0, 1, 2, 3, 4, 5] as i}
+      <a
+        href="#"
+        class="group relative overflow-hidden rounded-md bg-[#638ef4] p-4 font-medium text-white"
+      >
+        <div
+          class="absolute -top-36 left-0 h-full w-full rounded-md bg-[#f18776] opacity-0 transition-all duration-500 group-hover:top-0 group-hover:opacity-100"
+        />
+        <u class="relative decoration-wavy underline-offset-8">Lorem</u>
+        <p class="relative mt-8">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ea assumenda maxime, aut
+        </p>
+      </a>
+    {/each}
+  </section>
+</main>
---
+++ src/routes/Card.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { fly } from 'svelte/transition'
+  let hover = false
+</script>
+
+<button on:mouseover={() => (hover = true)}> asdf</button>
+{#if hover}
+  <a
+    transition:fly={{ y: 100, duration: 1000 }}
+    on:mouseover={() => (hover = true)}
+    on:mouseleave={() => (hover = false)}
+    href="#"
+    class="rounded-md bg-[#638ef4] p-4 font-medium text-white"
+  >
+    <div class="absolute h-full w-full bg-orange-500">a</div>
+    <u class="decoration-wavy underline-offset-8">Lorem</u>
+    <p class="mt-8">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ea assumenda maxime, aut
+    </p>
+  </a>
+{/if}
